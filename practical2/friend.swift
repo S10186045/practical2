c8d74a52614a6f425c15db06f36dfbd42792ba51
Victor <s10186045@connect.np.edu.sg>
2019-10-21 15:22:15 +0800

    Second Commit

---
+++ practical2/friend.swift
@@ -0,0 +1,23 @@
+//
+//  friend.swift
+//  practical2
+//
+//  Created by MAD2_P01 on 21/10/19.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+class Friend {
+    var firstName: String = ""
+    var lastName: String = ""
+    var age:Int = 0
+    
+    var description:String = ""
+    
+    init(firstname:String, lastname:String, age:Int){
+        self.firstName = firstname
+        self.lastName = lastname
+        self.age = age
+    }
+    
+    
+}
