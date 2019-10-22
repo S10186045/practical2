d5532ea82b9d74ca04c32d0028600d5b92c1d115
Victor <s10186045@connect.np.edu.sg>
2019-10-22 16:17:42 +0800

    Third Commit

--- practical2/ViewController.swift
+++ practical2/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    //creating list
+    var friendList:[Friend] = []
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //create new friend
+        let friend1 = Friend(firstname: "Amy", lastname: "Ann" , age:19)
+        let friend2 = Friend(firstname: "Billy", lastname: "Boo" , age:20)
+        
+        //appending created friend into the list
+        friendList.append(friend1)
+        friendList.append(friend2)
+        
     }
-
-
 }
 
